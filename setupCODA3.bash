b351cc83797a38ddce996a66126ed43077693c6a
Bryan Moffit <moffit@jlab.org>
2022-04-13 12:02:56 -0400

    Generalize the bash/tcsh setup scripts.
    - Cleanup on SBS specific stuff in other scripts.
    - Remove the shiftworker_table.cfg

--- setupCODA3.bash
+++ setupCODA3.bash
@@ -1,31 +1,37 @@
 # BASH Environment setup for SBS CODA 3
 
-# Set default CODA_SCRIPTS
-: ${CODA_SCRIPTS:=${HOME}/coda_scripts}
-
-export PATH=`echo $PATH | awk -v RS=: -v ORS=: '/coda/ {next} {print}' | sed 's/:*$//'`
-export LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | awk -v RS=: -v ORS=: '/coda/ {next} {print}' | sed 's/:*$//'`
-export CODA_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# if undefined, Use the PATH that contains these scripts as CODA_SCRIPTS
+: ${CODA_SCRIPTS:="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"}
 export PATH=${CODA_SCRIPTS}:${PATH}
 
-export CODA=/opt/idaq/coda/3.10_devel
+# if undefined, use /site
+: ${CODA:=/site/coda/3.10_devel}
+
+# CODA/.setup overwrites SESSION and EXPID, save them, or set the default values
+SAVE_SESSION=${SESSION:-session}
+SAVE_EXPID=${EXPID:-expid}
 
 if [ -f $CODA/.setup_bash ]; then
     source $CODA/.setup_bash
 else
-    source $CODA_SCRIPTS/coda3.10.setup_bash
+    source ${CODA_SCRIPTS}/coda3.10.setup_bash
 fi
 
-export SESSION=mott
-export EXPID=itfmdaq0
+# Restore SESSION and EXPID
+SESSION=${SAVE_SESSION}
+EXPID=${SAVE_EXPID}
+export SESSION EXPID
 
-export COOL_HOME=/opt/idaq/coda/cool.itfmdaq0
-export JAVA_HOME=/opt/idaq/jdk1.8.0_152
+# Default COOL_HOME and JAVA_HOME
+: ${COOL_HOME:=${HOME}/coda/cool}
+: ${JAVA_HOME:=${HOME}/jdk1.8.0_152}
+export COOL_HOME JAVA_HOME
 
-export REMEX_CMSG_HOST=itfmdaq0.acc.jlab.org
-export REMEX_CMSG_PASSWORD=${EXPID}
+# Default REMEX variables
+: ${REMEX_CMSG_HOST:=thishost.jlab.org}
+: ${REMEX_CMSG_PASSWORD:=${EXPID}}
+export REMEX_CMSG_HOST REMEX_CMSG_PASSWORD
 
-export CODA_COMPONENT_TABLE=$CODA_SCRIPTS/config/${EXPID}/coda_component_table.cfg
-
-# Add config scripts to path
-export PATH=${CODA_SCRIPTS}:$PATH
+# Default CODA_COMPONENT_TABLE
+: ${CODA_COMPONENT_TABLE:=${CODA_SCRIPTS}/config/${EXPID}/coda_component_table.cfg}
+export CODA_COMPONENT_TABLE
--- setupCODA3.tcsh
+++ setupCODA3.tcsh
@@ -3,16 +3,18 @@
 ########################################
 # User Specific Configuration
 ########################################
-setenv CODA /opt/idaq/coda/3.10_devel
+setenv CODA_SCRIPTS ${HOME}/coda/coda_scripts
+
+setenv CODA /site/coda/3.10_devel
 source $CODA/.setup
 
-setenv SESSION mott
-setenv EXPID itfmdaq0
+setenv SESSION session
+setenv EXPID expid
 
-setenv COOL_HOME /opt/idaq/coda/cool.itfmdaq0
-setenv JAVA_HOME /opt/idaq/jdk1.8.0_152
+setenv COOL_HOME ${HOME}/coda/cool
+setenv JAVA_HOME ${HOME}/jdk1.8.0_152
 
-setenv REMEX_CMSG_HOST itfmdaq0.acc.jlab.org
+setenv REMEX_CMSG_HOST thishost.jlab.org
 setenv REMEX_CMSG_PASSWORD ${EXPID}
 
 setenv CODA_COMPONENT_TABLE ${CODA_SCRIPTS}/config/${EXPID}/coda_component_table.cfg
